358f0603794a6b68e93486de9bd5022847c37856
XiferGames <97963597+XiferGames@users.noreply.github.com>
2022-09-13 12:38:49 +0200

    Create MagicButtonStartClick.cs

---
+++ MagicButtonStartClick.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.OnScreen;
+
+public class MagicButtonStartClick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+{
+    [SerializeField] Transform joystickT;
+    [SerializeField] OnScreenStick stick;
+
+    MyInputAction inputs;
+
+    void Start()
+    {
+        inputs = new MyInputAction();
+        inputs.Enable();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        joystickT.position = inputs.Player.ClickPosition.ReadValue<Vector2>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        stick.OnDrag(eventData);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        stick.OnPointerUp(eventData);
+    }
+}
